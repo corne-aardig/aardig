8344f3c5b7cbe2ed7223cfd1b54552e74af7cc72
corne-aardig <78164590+corne-aardig@users.noreply.github.com>
2025-03-17 14:19:00 +0100

    Update websitebyaardig.js

--- websitebyaardig.js
+++ websitebyaardig.js
@@ -1,26 +1,6 @@
-// ASCII art logo display script met kleiner logo
-const logo = `                                                     %%%%%  %%%%%%                
-                                                    %%%%%  %%%%%%                
-    @%%%%%%        %%%%%%%     @%%%%  @%%    %%%%%% %%%%%  %%%%%%    @%%%%@ %%%%%
- %%%%%%%%%%%%   @%%%%%%%%%%%@  @%%%%%%%%%  @%%%%%%%%%%%%%  %%%%%%  %%%%%%%%%%%%%%
-%%%%%%  %%%%%%  %%%%%@ @%%%%%  @%% @%%%%% @%%%%%%%%%% @%%  %%%%%% %%%%%%%%%%%%%%%
-  %%%%%%%@ %%%    %%%%%%%  %%  %%%%%%%    %%%%%%   %%%%%%  %%%%%% %%%%%%   %%%%%%
-%%%%%%%%%%%%%%  %%%%%%%%%%%%%  %%%%%%     %%%%%%   %%%%%%  %%%%%% %%%%%%   %%%%%%
-%%%%%  %%%%%%% %%%%%%  %%%%%%  %%%%%%     %%%%%%% %%%%%%%  %%%%%%  %%%%%%%%%% %%%
-%%%%%%%%%%%%%% %%%%%%%%%%%%%%@ %%%%%%      %%%%%%%%%%%%%%  %%%%%%   %%%%%%%%%%%%%
- @%%%%%%%%%%%%   %%%%%%@%%%%%@ @%%%%%       @%%%%%%%%%%%%  %%%%%%  %%%%%%% %%%%%%
-                                                                   %%%%%%%%%%%%%%
-                                                                   @%%%%%%%%%%%@ 
-      %%%            %%                                                @%%%%     
-      %%%%%%%%%%%%%%%%%%                                                         
-         %%%%%%%%%%%@`;
-
-// Functie om logo in een kader te plaatsen met tekst
-function createFramedLogo(title = "Developed by aardig", footer = "www.aardig.amsterdam") {
-  const lines = logo.split('\n');
-  const maxWidth = Math.max(...lines.map(line => line.length));
+function createTextFrame(title = "Developed by aardig", footer = "www.aardig.amsterdam", width = 40) {
+  width = Math.max(width, title.length + 4, footer.length + 4);
   
-  // Kader karakters
   const topLeft = '╔';
   const topRight = '╗';
   const bottomLeft = '╚';
@@ -28,33 +8,23 @@ function createFramedLogo(title = "Developed by aardig", footer = "www.aardig.am
   const horizontal = '═';
   const vertical = '║';
   
-  // Maak bovenkant van kader
-  let result = topLeft + horizontal.repeat(maxWidth + 2) + topRight + '\n';
+  let result = topLeft + horizontal.repeat(width - 2) + topRight + '\n';
   
-  // Voeg logo toe met verticale randen
-  lines.forEach(line => {
-    const padding = ' '.repeat(maxWidth - line.length);
-    result += vertical + ' ' + line + padding + ' ' + vertical + '\n';
-  });
+  result += vertical + ' '.repeat(width - 2) + vertical + '\n';
   
-  // Voeg lege regel toe voor wat ruimte
-  result += vertical + ' ' + ' '.repeat(maxWidth) + ' ' + vertical + '\n';
+  const titlePaddingLeft = Math.floor((width - 2 - title.length) / 2);
+  const titlePaddingRight = width - 2 - title.length - titlePaddingLeft;
+  result += vertical + ' '.repeat(titlePaddingLeft) + title + ' '.repeat(titlePaddingRight) + vertical + '\n';
   
-  // Voeg titel toe (gecentreerd)
-  const titlePaddingLeft = Math.floor((maxWidth - title.length) / 2);
-  const titlePaddingRight = maxWidth - title.length - titlePaddingLeft;
-  result += vertical + ' ' + ' '.repeat(titlePaddingLeft) + title + ' '.repeat(titlePaddingRight) + ' ' + vertical + '\n';
+  const footerPaddingLeft = Math.floor((width - 2 - footer.length) / 2);
+  const footerPaddingRight = width - 2 - footer.length - footerPaddingLeft;
+  result += vertical + ' '.repeat(footerPaddingLeft) + footer + ' '.repeat(footerPaddingRight) + vertical + '\n';
   
-  // Voeg footer toe (gecentreerd)
-  const footerPaddingLeft = Math.floor((maxWidth - footer.length) / 2);
-  const footerPaddingRight = maxWidth - footer.length - footerPaddingLeft;
-  result += vertical + ' ' + ' '.repeat(footerPaddingLeft) + footer + ' '.repeat(footerPaddingRight) + ' ' + vertical + '\n';
+  result += vertical + ' '.repeat(width - 2) + vertical + '\n';
   
-  // Maak onderkant van kader
-  result += bottomLeft + horizontal.repeat(maxWidth + 2) + bottomRight;
+  result += bottomLeft + horizontal.repeat(width - 2) + bottomRight;
   
   return result;
 }
 
-// Toon het logo in de console met monospace lettertype voor correcte weergave
-console.log("%c" + createFramedLogo(), "font-family: monospace; white-space: pre;");
+console.log("%c" + createTextFrame(), "font-family: monospace; white-space: pre;");
